2e89f67f231575362074a8db5b0dac4c49d5ccf9
Eduard Teslaru <eduard.teslaru@gmail.com>
2025-12-13 19:40:28 -0500

    Add coin system, main menu, and pause functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,23 +10,35 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 4f;           // How fast the player moves left/right
 
     // --- Jump variables ---
-    public float jumpForce = 8f;           // Base jump force (vertical speed)
+    public float jumpForce = 7.5f;
+    public float jumpContinuesForce = 1f;// Base jump force (vertical speed)
     public int extraJumpsValue = 1;        // How many extra jumps allowed (1 = double jump, 2 = triple jump)
     private int extraJumps;                // Counter for jumps left
 
     public Transform groundCheck;          // Empty child object placed at the player's feet
     public float groundCheckRadius = 0.2f; // Size of the circle used to detect ground
-    public LayerMask groundLayer;          // Which layer counts as "ground" (set in Inspector)
+    public LayerMask groundLayer;
+    // Which layer counts as "ground" (set in Inspector)
+    public float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
 
     // --- Internal state ---
     private Rigidbody2D rb;                // Reference to the Rigidbody2D component
-    private bool isGrounded;               // True if player is standing on ground
+    private bool isGrounded;
+
+    // True if player is standing on ground
+    public float jumpBufferTime = 0.15f;
+    private float jumpBufferCounter;
+    // Addng audio
+    private AudioSource audioSource;
+    public AudioClip jumoClip;
 
     void Start()
     {
         // Grab references once at the start
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -48,18 +61,33 @@ public class PlayerController : MonoBehaviour
         // Reset extra jumps when grounded
         if (isGrounded)
         {
+            coyoteTimeCounter = coyoteTime;
             extraJumps = extraJumpsValue;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;    
         }
 
         // --- Jump & Double Jump ---
         // If Space is pressed:
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (jumpBufferCounter > 0f)
         {
-            if (isGrounded)
+            if (coyoteTimeCounter > 0f)
             {
                 // Normal jump
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
                 SoundManager.Instance.PlaySFX("JUMP");
+                coyoteTimeCounter = 0f;
+                jumpBufferCounter = 0f;
             }
             else if (extraJumps > 0)
             {
@@ -67,12 +95,28 @@ public class PlayerController : MonoBehaviour
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
                 extraJumps--; // Reduce available extra jumps
                 SoundManager.Instance.PlaySFX("JUMP");
+                jumpBufferCounter = 0f;
             }
+        }
+        if(Input .GetKey(KeyCode.Space) &&  rb.linearVelocityY > 0)
+        {
+            rb.AddForceY(jumpContinuesForce);
         }
 
         // --- Animations ---
         SetAnimation(moveInput);
+        if(rb.linearVelocityY < 0)
+        {
+            rb.gravityScale = 2f;
+        }
+        else
+        {
+            rb.gravityScale = 1.5f;
+
+        }
+
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("BouncePad"))
@@ -83,7 +127,14 @@ public class PlayerController : MonoBehaviour
             // Sound effect (optional)
            
         }
+        if (collision.gameObject.tag == "Banana")
+        {
+            extraJumps = 2;
+            Destroy(collision.gameObject);
+        }
     }
+    
+
     private void SetAnimation(float moveInput)
     {
         // Handle animations based on grounded state and movement
@@ -109,5 +160,14 @@ public class PlayerController : MonoBehaviour
                 animator.Play("Player_Fall"); // Fall animation when moving downward
             }
         }
+    }
+    public void PlaySFX(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+    internal void PlaySFX(AudioClip coinClip, float v)
+    {
+        throw new NotImplementedException();
     }
 }
